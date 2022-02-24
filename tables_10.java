f97710305562ec90ef0cfc00f7d9b41885abf520
ManishBhuwad <manishbhuwad11@gmail.com>
2022-02-25 00:15:09 +0530

    added commit

--- tables_10.java
+++ tables_10.java
@@ -4,6 +4,7 @@ public class tables_10 {
         for(int i= 1; i<=10; i++){
             for(int j=1; j<=10; j++){
                 int a= i*j;
+                //printed comment to test
                 System.out.println(i+"*"+j+"="+a);
             }
             
