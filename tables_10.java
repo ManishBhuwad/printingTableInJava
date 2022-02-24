ec45aaac09ab39007363909e2f613915198fac54
ManishBhuwad <manishbhuwad11@gmail.com>
2022-02-25 00:16:12 +0530

    removed commit

--- tables_10.java
+++ tables_10.java
@@ -4,7 +4,6 @@ public class tables_10 {
         for(int i= 1; i<=10; i++){
             for(int j=1; j<=10; j++){
                 int a= i*j;
-                //printed comment to test
                 System.out.println(i+"*"+j+"="+a);
             }
             
