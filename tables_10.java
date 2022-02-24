fd08698fa1a0fc8505ced9139cf2ddd563f568d6
ManishBhuwad <manishbhuwad11@gmail.com>
2022-02-25 00:08:23 +0530

    printed table

---
+++ tables_10.java
@@ -0,0 +1,14 @@
+public class tables_10 {
+    public static void main(String[] args) {
+        
+        for(int i= 1; i<=10; i++){
+            for(int j=1; j<=10; j++){
+                int a= i*j;
+                System.out.println(i+"*"+j+"="+a);
+            }
+            
+        }
+    }
+}
+
+        
